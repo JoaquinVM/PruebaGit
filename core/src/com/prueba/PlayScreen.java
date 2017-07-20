8a243d921012535d0ef170f084e9959ec6417977
Joaco99 <viscafe38@gmail.com>
2017-07-20 10:56:10 -0400

    hi

--- core/src/com/prueba/PlayScreen.java
+++ core/src/com/prueba/PlayScreen.java
@@ -1,6 +1,8 @@
 package com.prueba;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -21,9 +23,12 @@ public class PlayScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
     }
 
     @Override
     public void dispose() {
+        stage.dispose();
     }
 }
