937187a598f6054a1163bcdd3f6d889c7182df97
Joaco99 <viscafe38@gmail.com>
2017-07-20 13:07:10 -0400

    hi

--- core/src/com/prueba/util/Constants.java
+++ core/src/com/prueba/util/Constants.java
@@ -6,11 +6,11 @@ package com.prueba.util;
 
 public class Constants {
     public static final int BOX_MAX_CAPACITY = 100;
-    public static float WORLD_WIDTH = 600;
-    public static float WORLD_HEIGHT = 400;
-    public static int INITIAL_BOX_CAPACITY = 0;
-    public static int BOX_INCREMENT = 20;
-    public static float BOX_AND_FONT_DISTANCE = 20;
+    public static final float WORLD_WIDTH = 600;
+    public static final float WORLD_HEIGHT = 400;
+    public static final int INITIAL_BOX_CAPACITY = 0;
+    public static final int BOX_INCREMENT = 20;
+    public static final float BOX_AND_FONT_DISTANCE = 20;
     
     public static final String BOX_TEXTURE = "box.png";
     
