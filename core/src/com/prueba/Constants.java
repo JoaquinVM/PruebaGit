0d2e9ad614df5856cb93daf32aaacdde6e166e39
Joaco99 <viscafe38@gmail.com>
2017-07-20 10:54:11 -0400

    hi

---
+++ core/src/com/prueba/Constants.java
@@ -0,0 +1,10 @@
+package com.prueba;
+
+/**
+ * Created by Joaco99 on 20/07/2017.
+ */
+
+public class Constants {
+    public static float WORLD_WIDTH = 600;
+    public static float WORLD_HEIGHT = 400;
+}
--- core/src/com/prueba/PlayScreen.java
+++ core/src/com/prueba/PlayScreen.java
@@ -2,6 +2,8 @@ package com.prueba;
 
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by Joaco99 on 20/07/2017.
@@ -9,9 +11,12 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class PlayScreen extends ScreenAdapter {
     Stage stage;
+    Viewport viewport;
 
     @Override
     public void show() {
+        stage = new Stage();
+        viewport = new ExtendViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
     }
 
     @Override
