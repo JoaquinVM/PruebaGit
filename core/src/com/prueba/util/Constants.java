efd663c915bd85d99db59b22ce17fdd6c87eb618
Groxar <lrdolucho@gmail.com>
2017-07-20 13:57:02 -0400

    base shit for the shit

--- core/src/com/prueba/util/Constants.java
+++ core/src/com/prueba/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final float WORLD_WIDTH = 600;
     public static final float WORLD_HEIGHT = 400;
     public static final int BOX_MAX_CAPACITY = 100;
-    public static final float BOX_SPAWN_RATE = 1;
+    public static final float INITIAL_BOX_SPAWN_RATE = 1;
     public static final float BOX_SPAWN_RATE_INCREMENT = 0.2f;
     public static final int INITIAL_BOX_CAPACITY = 0;
     public static final int BOX_INCREMENT = 20;
