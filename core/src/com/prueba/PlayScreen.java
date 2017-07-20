a7047e90806673118da647261832ae479491d37a
Joaco99 <viscafe38@gmail.com>
2017-07-20 10:51:51 -0400

    hi

--- core/src/com/prueba/PlayScreen.java
+++ core/src/com/prueba/PlayScreen.java
@@ -1,10 +1,24 @@
 package com.prueba;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
  * Created by Joaco99 on 20/07/2017.
  */
 
 public class PlayScreen extends ScreenAdapter {
+    Stage stage;
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
