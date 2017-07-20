2c7f4b34128fd0b5672627521dada3b0c4631e50
Groxar <lrdolucho@gmail.com>
2017-07-20 13:52:36 -0400

    constants

--- core/src/com/prueba/util/Constants.java
+++ core/src/com/prueba/util/Constants.java
@@ -1,17 +1,15 @@
 package com.prueba.util;
 
-/**
- * Created by Joaco99 on 20/07/2017.
- */
-
 public class Constants {
-    public static final int BOX_MAX_CAPACITY = 100;
     public static final float WORLD_WIDTH = 600;
     public static final float WORLD_HEIGHT = 400;
+    public static final int BOX_MAX_CAPACITY = 100;
+    public static final float BOX_SPAWN_RATE = 1;
+    public static final float BOX_SPAWN_RATE_INCREMENT = 0.2f;
     public static final int INITIAL_BOX_CAPACITY = 0;
     public static final int BOX_INCREMENT = 20;
     public static final float BOX_AND_FONT_DISTANCE = 20;
-    
+
     public static final String BOX_TEXTURE = "box.png";
-    
+
 }
