bc4a61c860f9c2cef1487064cccfb3bd5b3f97d3
Groxar <lrdolucho@gmail.com>
2017-07-20 11:09:38 -0400

    Scene and box image

---
+++ core/src/com/prueba/GameScene.java
@@ -0,0 +1,14 @@
+package com.prueba;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by Luchox on 20/07/2017.
+ */
+public class GameScene extends ScreenAdapter {
+
+    @Override
+    public void render(float delta) {
+
+    }
+}
